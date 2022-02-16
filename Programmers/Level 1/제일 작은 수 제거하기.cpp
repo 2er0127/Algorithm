33cfd37aed1c2a048344d4f02a5e48439ca01b18
princesssuna <1009suna@gmail.com>
2022-02-17 00:01:15 +0900

    Update 제일 작은 수 제거하기

--- Programmers/Level 1/제일 작은 수 제거하기.cpp
+++ Programmers/Level 1/제일 작은 수 제거하기.cpp
@@ -9,10 +9,10 @@ vector<int> solution(vector<int> arr) {
     int min_ele = *min_element(answer.begin(), answer.end());
     
     for(int i = 0; i < arr.size(); i++) {
-        if(answer.empty() == true) {
+          /* if(answer.empty() == true) {
             answer.push_back(-1);
             return answer;
-        }
+        } */
         
         if(answer.size() == 1) {
             answer.clear();
